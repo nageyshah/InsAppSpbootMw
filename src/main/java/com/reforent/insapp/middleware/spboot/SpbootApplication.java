9ff0a63462d6a22f398f60bb5ecc44939f4cb18d
nageyshah <nagey.shaheen@gmail.com>
2017-01-11 14:59:27 -0500

    initial commit

---
+++ src/main/java/com/reforent/insapp/middleware/spboot/SpbootApplication.java
@@ -0,0 +1,12 @@
+package com.reforent.insapp.middleware.spboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootApplication.class, args);
+	}
+}
